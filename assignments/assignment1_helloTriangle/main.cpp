d8839d25b1a99deaf8fc6d06a6117b5cdca1e772
Ethan Chevalier <ethan.chevalier@mymail.champlain.edu>
2023-09-11 19:07:11 -0400

    Working build with no refactoring

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -7,9 +7,40 @@
 
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
+const char* vertexShaderSource = R"(
+	#version 450
+	layout(location = 0) in vec3 vPos;
+	layout(location = 1) in vec4 vColor;
+	out vec4 Color;
+	uniform float _Time;
+	void main(){
+		Color = vColor;
+		vec3 offset = vec3(0,sin(vPos.x + _Time),0)*0.5;
+		gl_Position = vec4(vPos + offset,1.0);
+	}
+)";
+const char* fragmentShaderSource = R"(
+	#version 450
+	out vec4 FragColor;
+	in vec4 Color;
+	uniform float _Time;
+	void main(){
+		FragColor = Color * abs(sin(_Time));
+	}
+)";
 
 int main() {
 	printf("Initializing...");
+
+	float vertices[21] = {
+		//x   //y  //z   //r  //g  //b  //a
+		-0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0, //Point left
+		 0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, //Point right
+		 0.0,  0.5, 0.0, 0.0, 0.0, 1.0, 1.0  //Point center
+	};
+
+
+
 	if (!glfwInit()) {
 		printf("GLFW failed to init!");
 		return 1;
@@ -27,10 +58,89 @@ int main() {
 		return 1;
 	}
 
+	//New buffer
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocate space for + send vertex data to GPU.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	//Pull data from vbo
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Define position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)0);
+	glEnableVertexAttribArray(0);
+	//Define color
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)(sizeof(float) * 3));
+	glEnableVertexAttribArray(1);
+
+	//unsigned int createVAO(float* vertexData, int numVertices);
+
+
+	//Create new vertex shader and compile
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	
+	//Create new fragment shader and compile
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	int success;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		//512 is an arbitrary length, but should be plenty of characters for our error message.
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("Failed to compile vertex shader: %s", infoLog);
+	}
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		//512 is an arbitrary length, but should be plenty of characters for our error message.
+		char infoLog[512];
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		printf("Failed to compile fragment shader: %s", infoLog);
+	}
+
+	unsigned int shaderProgram = glCreateProgram();
+	//Attach each stage
+	glAttachShader(shaderProgram, vertexShader);
+	//glAttachShader(shaderProgram, geometryShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	//Link all the stages together
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		char infoLog[512];
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		printf("Failed to link shader program: %s", infoLog);
+	}
+	//The linked program now contains our compiled code, so we can delete these intermediate objects
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vao);
+		//The current time in seconds this frame
+		float time = (float)glfwGetTime();
+		//Get the location of the uniform by name
+		int timeLocation = glGetUniformLocation(shaderProgram, "_Time");
+		//Set the value of the variable at the location
+		glUniform1f(timeLocation, time);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
